0e9889a6f756aaf8dab36cb84d6d6dc019f930cb
Mariela <mariela.angelova99@gmail.com>
2023-04-28 16:23:45 +0300

    footer and navbar

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  exports: [
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
+  ]
+})
+export class CoreModule { }
